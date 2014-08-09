21587bc7a05c5c1d759d881f2768c237990d9fcc
Daniel Woods <daniel.p.woods@gmail.com>
2014-08-08 20:48:10 -0700

    DEMO #1 - Node app with Nashorn

---
+++ src/main/java/midwestjs/nashorn/Main.java
@@ -0,0 +1,27 @@
+package midwestjs.nashorn;
+
+import com.oracle.avatar.js.Server;
+
+import java.io.File;
+import java.lang.reflect.Field;
+
+public class Main {
+
+    static {
+        try {
+            final Field sysPathsField = ClassLoader.class.getDeclaredField("sys_paths");
+            sysPathsField.setAccessible(true);
+            sysPathsField.set(null, null);
+            System.setProperty("java.library.path",
+                    new File(Main.class.getResource("/libavatar-js.so").getPath())
+                            .getParentFile().getCanonicalPath());
+            System.loadLibrary("avatar-js");
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        }
+    }
+
+    public static void main(String[] args) throws Throwable {
+        Server.main(Main.class.getResource("/app.js").getPath());
+    }
+}
